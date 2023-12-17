6e64c2b06251946e2be5a507ed5b018e014787f2
MarynaFundament <marina.fundament@ukr.net>
2023-12-17 14:04:08 +0200

    mobile version

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,36 @@
       refs.modal.classList.toggle("is_hidden");
     }
   })();
+
+
+
+  (() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+      body: document.querySelector("body"),
+      menuList: document.querySelector(".menu-list"),
+
+    };
+
+    console.log(refs.openMenuBtn);
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.menuList.addEventListener("click", removeMenu);
+
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is_hidden");
+      refs.body.classList.toggle("no-scroll");
+    }
+
+    function removeMenu() {
+      refs.menu.classList.add("is_hidden");
+      refs.body.classList.remove("no-scroll");
+    }
+
+
+
+  })();
